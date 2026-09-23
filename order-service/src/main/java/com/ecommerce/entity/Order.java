7f953f2b2789220ceadf446155b698e98645b1b5
Sumit <sumitkumarjha770@gmail.com>
2026-09-23 22:05:58 +0530

    created the order service implemented the order

---
+++ order-service/src/main/java/com/ecommerce/entity/Order.java
@@ -0,0 +1,110 @@
+package com.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+	
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Long customerId;
+	
+	private String customerName;
+	
+	private Long productId;
+	
+	private String productName;
+	
+	private Integer quantity;
+	
+	private Double amount;
+	
+	private String delieryAddress;
+	
+	private String status;
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public String getDelieryAddress() {
+		return delieryAddress;
+	}
+
+	public void setDelieryAddress(String delieryAddress) {
+		this.delieryAddress = delieryAddress;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+
+}
